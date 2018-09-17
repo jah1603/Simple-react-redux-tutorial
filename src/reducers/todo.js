375e1436fe69d38d5839d675e87e0d84fd29b77f
jah1603 <hendersonjamesalexander@gmail.com>
2018-09-17 12:50:10 +0100

    adds todo.js

--- src/reducers/todo.js
+++ src/reducers/todo.js
@@ -3,5 +3,30 @@ import {
   TOGGLE_TODO
 } from '../actions/actions'
 
+function todos(state = [], action) {
+  switch (action.type) {
+    case ADD_TODO:
+      return [
+        ...state,
+        {
+          text: action.text,
+          completed: false
+        }
+      ]
+    case TOGGLE_TODO:
+      return state.map((todo, index) => {
+        if (index == action.index) {
+          return Object.assign({}, todo, {
+            completed: !todo.completed
+          })
+        }
+        return todo
+      })
+      default:
+        return state
+  }
+}
+
+
 
 export default todo;
