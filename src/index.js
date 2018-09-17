a3d8cf6ad7adb779f2ce8d343a87effe3c9e586e
jah1603 <hendersonjamesalexander@gmail.com>
2018-09-17 21:24:44 +0100

    adds further content

--- src/index.js
+++ src/index.js
@@ -2,11 +2,9 @@ import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
 import { createStore } from 'redux'
-import todoApp from './reducers'
-import App from './components/App'
-​
+import todoApp from './reducers/index'
+import App from 'App'
 const store = createStore(todoApp)
-​
 render(
   <Provider store={store}>
     <App />
