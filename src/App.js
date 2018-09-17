25d8277c30ab1c49e21962a1ebb68b53d373f820
jah1603 <hendersonjamesalexander@gmail.com>
2018-09-17 13:02:15 +0100

    adds app.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-      
-      </div>
-    );
-  }
-}
-
-export default App;
+import React from 'react'
+import Footer from './components/Footer'
+import AddTodo from './containers/AddTodo'
+import VisibleTodoList from './containers/VisibleToDoList'
+​
+const App = () => (
+  <div>
+    <AddTodo />
+    <VisibleTodoList />
+    <Footer />
+  </div>
+)
+​
+export default App
