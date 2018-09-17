1c1177bf22fa9cf6b90ba46a3fc15e3de1215893
jah1603 <hendersonjamesalexander@gmail.com>
2018-09-17 12:31:32 +0100

    adds completed actions.js

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -8,4 +8,14 @@ export const = VisibilityFilters = {
   SHOW_ACTIVE: 'SHOW_ACTIVE'
 }
 
-export function addTodo(text)
+export function addTodo(text){
+  return { type: ADD_TODO, text }
+}
+
+export function toggleTodo(text){
+  return { type: TOGGLE_TODO, text }
+}
+
+export function visibilityFilter(filter){
+  return { type: SET_VISIBILITY_FILTER, filter }
+}
